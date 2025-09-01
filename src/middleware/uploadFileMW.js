b968a8cfa50558dcac2efcdd67808d165c136281
miraweetech <miraweetech@gmail.com>
2025-09-01 11:27:59 +0530

    login api

--- src/middleware/uploadFileMW.js
+++ src/middleware/uploadFileMW.js
@@ -2,7 +2,6 @@ import multer from "multer";
 import path from "path";
 import fs from "fs";
 
-// Multer storage config
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     const uploadPath = path.join(process.cwd(), "src", "assets", "uploads");
